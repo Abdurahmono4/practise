4274e5804b89bd8ef232b5f3b11add86f8748e2b
Abdurahmono4 <olimjonovrahmonjon7@gmail.com>
2024-04-18 16:51:10 +0500

    new changes

---
+++ src/js/about.js
@@ -0,0 +1,19 @@
+import "../css/main.css";
+import "./mode";
+
+//request
+import request from "./request";
+import { createCounryInfo } from "./updateUI";
+
+const querySting = window.location.search;
+const urlParams = new URLSearchParams(querySting);
+const country = urlParams.get("country");
+const countryAPI = `https://restcountries.com/v3.1/${country}`;
+
+request(countryAPI)
+  .then((data) => {
+    createCounryInfo(data[0]);
+  })
+  .catch((err) => {
+    alert(err.message);
+  });
